f92d262443fd60f5cf8b899534c4c4d753756c36
nncrayen <nncrayen@gmail.com>
2021-12-25 17:35:17 -0300

    Header fixed classes and queries

--- js/main.js
+++ js/main.js
@@ -4,12 +4,9 @@ $(document).ready(function(){
 	//$("header").load("/./component/header.html");  
 	//$("footer").load("/./component/footer.html"); 
 
-	//$("#btn_nav").click(function() {
+	$("#btn_nav").click(function() {
 
 
-
-  		//$(this).toggle('src','img/icons/cross-icon.png');
-  		//$("#top_nav").toggleClass('mb_nav');
-	//});
+	});
 
 })
