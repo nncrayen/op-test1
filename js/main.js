747b7d48e4bbbacdcf400a1fade90c74d2910e6d
nncrayen <nncrayen@gmail.com>
2021-12-24 22:36:50 -0300

    testing gh-pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 	
 	// Posts
-	$("header").load("/./component/header.html");  
+	//$("header").load("/./component/header.html");  
 	//$("footer").load("/./component/footer.html"); 
+
+	//$("#btn_nav").click(function() {
+
+
+
+  		//$(this).toggle('src','img/icons/cross-icon.png');
+  		//$("#top_nav").toggleClass('mb_nav');
+	//});
+
 })
