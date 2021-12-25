b947b27a94977d731782c0fa35e8fbed262efc57
nncrayen <nncrayen@gmail.com>
2021-12-25 20:32:56 -0300

    Landing (principal section) fixed classes and queries

--- js/main.js
+++ js/main.js
@@ -4,9 +4,5 @@ $(document).ready(function(){
 	//$("header").load("/./component/header.html");  
 	//$("footer").load("/./component/footer.html"); 
 
-	$("#btn_nav").click(function() {
-
-
-	});
 
 })
